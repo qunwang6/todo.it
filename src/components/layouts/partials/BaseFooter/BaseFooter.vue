072771ce220d97486ba1b1165b49158b3cc5502f
qunwang6 <qunwang6@gmail.com>
2023-09-20 17:41:51 +0800

    aaaa

--- src/components/layouts/partials/BaseFooter/BaseFooter.vue
+++ src/components/layouts/partials/BaseFooter/BaseFooter.vue
@@ -3,21 +3,8 @@
     <div
       class="container flex flex-col items-center justify-center mx-auto p-5 pb-10 text-sm text-center text-gray-500"
     >
-      <p>
-        made with <span class="text-red-500 text-md">❤</span> by
-        <a href="https://github.com/begprod" class="text-blue-600">begprod</a>
-      </p>
-      <p>
-        <a
-          class="text-blue-600"
-          href="https://github.com/begprod/todo.it#install-application-on-your-device"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Install app on device instruction
-          <span class="inline-block ml-2 -rotate-12 origin-center">→</span>
-        </a>
-      </p>
+ 
+
     </div>
   </footer>
 </template>
